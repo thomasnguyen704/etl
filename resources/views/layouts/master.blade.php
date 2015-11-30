85bf369cee20986ef186ff98cc70fc20f43a4ded
Thomas Nguyen <thomasnguyen@fas.harvard.edu>
2015-11-29 21:09:48 -0500

    edit title login index page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title> @yield('title', 'Thomas Nguyen'); </title>
+		<title> @yield('title') | Thomas Nguyen </title>
 		<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" type="text/css">
 		<link rel="stylesheet" href="/tablesorter/css/theme.default.css" type="text/css">
 		<style> 
